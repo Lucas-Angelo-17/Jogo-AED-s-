a7c3568f6c994bcac9774756613f802188672f28
Lucas-Angelo-17 <lucas.angelo17@gmail.com>
2021-06-27 14:43:55 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,13 @@
 
 using namespace std;
 
+int main {
+    printf("Role Play Game -Classic- \n");
+    printf ("****************************\n");
+    printf ("Role Play Game -Classic- funciona por turnos");
+    printf ("a cada turno que passa ambos os jogadores tem o direito de ataquer utilizando magias ou armas \n");
+}
+
 Personagem *player1, *player2;
 
 player1 = new Personagem("guerreiro.txt");
