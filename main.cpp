3e953a3a3a4bfed1ac34fe1edb9f370240d1e772
Gabriel Pimentel Tabatinga <80406092+Gabriel-Tabatinga@users.noreply.github.com>
2021-06-28 09:28:50 -0300

    organizei

--- main.cpp
+++ main.cpp
@@ -53,91 +53,91 @@ int main() {
     printf("8-Zumbi\n");
     //switch case para cada caso para criação de variavel
     do{
-    printf("\nPersonagem do Jogador 1: ");
-    scanf("%d", &personagem1);
-    switch (personagem1)
-    {
-    case 1:
-        printf("*Guerreiro*\n");
-        player1 = new Guerreiro();
-        break;
-    case 2:
-        printf("*Ladrão*\n");
-        player1 = new Ladrao();
-        break;
-    case 3:
-        printf("*Mago*\n");
-        player1 = new Mago();
-        break;
-    case 4:
-        printf("*Paladino*\n");
-        player1 = new Paladino();
-        break;
-    case 5:
-        printf("*Animal*\n");
-        player1 = new Animal();
-        break;
-    case 6:
-        printf("*Troll*\n");
-        player1 = new Troll();
-        break;
-    case 7:
-        printf("*Dagrão*\n");
-        player1 = new Dragao();
-        break;
-    case 8:
-        printf("*Zumbi*\n");
-        player1 = new Zumbi();
-        break;
-    
-    default:
-        printf("Opção inválida\n");
-        break;
-    }
+        printf("\nPersonagem do Jogador 1: ");
+        scanf("%d", &personagem1);
+        switch (personagem1)
+        {
+        case 1:
+            printf("*Guerreiro*\n");
+            player1 = new Guerreiro();
+            break;
+        case 2:
+            printf("*Ladrão*\n");
+            player1 = new Ladrao();
+            break;
+        case 3:
+            printf("*Mago*\n");
+            player1 = new Mago();
+            break;
+        case 4:
+            printf("*Paladino*\n");
+            player1 = new Paladino();
+            break;
+        case 5:
+            printf("*Animal*\n");
+            player1 = new Animal();
+            break;
+        case 6:
+            printf("*Troll*\n");
+            player1 = new Troll();
+            break;
+        case 7:
+            printf("*Dagrão*\n");
+            player1 = new Dragao();
+            break;
+        case 8:
+            printf("*Zumbi*\n");
+            player1 = new Zumbi();
+            break;
+        
+        default:
+            printf("Opção inválida\n");
+            break;
+        }
     }while(personagem1 <= 0 || 8 < personagem1);
 
     do{
-    printf("\nPersonagem do Jogador 2: ");
-    scanf("%d", &personagem2);
-    switch (personagem2)
-    {
-    case 1:
-        printf("*Guerreiro*\n");
-        player2 = new Guerreiro();
-        break;
-    case 2:
-        printf("*Ladrão*\n");
-        player2 = new Ladrao();
-        break;
-    case 3:
-        printf("*Mago*\n");
-        player2 = new Mago();
-        break;
-    case 4:
-        printf("*Paladino*\n");
-        player2 = new Paladino();
-        break;
-    case 5:
-        printf("*Animal*\n");
-        player2 = new Animal();
-        break;
-    case 6:
-        printf("*Troll*\n");
-        player2 = new Troll();
-        break;
-    case 7:
-        printf("*Dagrão*\n");
-        player2 = new Dragao();
-        break;
-    case 8:
-        printf("*Zumbi*\n");
-        player2 = new Zumbi();
-        break;
-    
-    default:
-        printf("Opção inválida\n");
-        break;
-    }
+        printf("\nPersonagem do Jogador 2: ");
+        scanf("%d", &personagem2);
+        switch (personagem2)
+        {
+        case 1:
+            printf("*Guerreiro*\n");
+            player2 = new Guerreiro();
+            break;
+        case 2:
+            printf("*Ladrão*\n");
+            player2 = new Ladrao();
+            break;
+        case 3:
+            printf("*Mago*\n");
+            player2 = new Mago();
+            break;
+        case 4:
+            printf("*Paladino*\n");
+            player2 = new Paladino();
+            break;
+        case 5:
+            printf("*Animal*\n");
+            player2 = new Animal();
+            break;
+        case 6:
+            printf("*Troll*\n");
+            player2 = new Troll();
+            break;
+        case 7:
+            printf("*Dagrão*\n");
+            player2 = new Dragao();
+            break;
+        case 8:
+            printf("*Zumbi*\n");
+            player2 = new Zumbi();
+            break;
+        
+        default:
+            printf("Opção inválida\n");
+            break;
+        }
     }while(personagem2 <= 0 || 8 < personagem2);
 
     int aux1, aux2;
